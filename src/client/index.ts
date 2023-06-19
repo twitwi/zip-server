6c7be0421f8927769c248ce753c4c8fa29409db2
Rémi Emonet <remi.emonet@reverse--com.heeere>
2023-06-19 16:38:59 +0200

    Relative path to be location insensitive

--- src/client/index.ts
+++ src/client/index.ts
@@ -19,12 +19,12 @@ navigator.serviceWorker.ready.then(() => {
             switch (message.data.type) {
                 case 'LOADED':
                     console.log('got message: LOADED');
-                    const response = await fetch(`/${id}/data/b41a6b6af21edf559e0f5a473b5ca6f5447b1d7e.zip`);
+                    const response = await fetch(`./${id}/data/b41a6b6af21edf559e0f5a473b5ca6f5447b1d7e.zip`);
                     const blob = await response.blob();
                     const url = URL.createObjectURL(blob);
                     console.log('Link to zip:', url)
 
-                    document.location.href = `/${id}/`;
+                    document.location.href = `./${id}/`;
                     // const iframe = document.createElement('iframe');
                     // iframe.src = `/${id}/`;
                     // let iframeContainer = document.querySelector('#iframe-container');
